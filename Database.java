05323205497c62c6665208745357dd9aba30baaa
Kiefhoy <khoy@purdue.edu>
2024-10-31 19:09:59 -0400

    Added two Strings for file path and some methods

--- Database.java
+++ Database.java
@@ -1,1 +1,23 @@
-test test
+import java.util.*;
+import java.io.*;
+public class Database {
+    ArrayList<User> users;
+    String userFile;
+    String conversationFile;
+
+    public Database(String users, String conversations) {
+        userFile = users;
+        conversationFile = conversations;
+    }
+    
+    public boolean readUserFile(String) {
+        try {
+            File f = new File()
+            BufferedReader br = new BufferedReader(FileReader(f));
+            
+        } catch (IOException e) {
+
+        }
+    }
+}
+
