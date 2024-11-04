66e46f46c147acbf7735855cc88331eba65e0e94
Marveon Morris <117520854+aapano@users.noreply.github.com>
2024-11-03 23:36:36 -0500

    Convo & Search

--- Message.java
+++ Message.java
@@ -1,3 +1,26 @@
+import java.io.*;
+import java.util.Scanner;
+
+
 public class Message {
 
+    String m = "";
+
+    public void sendMessage(m){
+        while (m = null){
+            System.out.println("Enter a Message")
+        }
+        m = scanner.nextLine();
+        System.out.println(m);
+
+    }
+
+    public static void deleteMessage{
+        boolean delete = false();
+        if (boolean delete = true){
+            
+        }
+    }
+
+    public static 
 }
