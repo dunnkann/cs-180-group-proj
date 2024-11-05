9fd3bc61593658eb746c293078990996c7fc24ee
Kiefhoy <khoy@purdue.edu>
2024-11-04 23:05:46 -0500

    Create RunLocalTest.java
    A place to put all the test cases

---
+++ RunLocalTest.java
@@ -0,0 +1,24 @@
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+/**
+ * A framework to run public test cases for Phase 1.
+ *
+ * 
+ *
+ * @author Duncan,Marveon,Kiefer,Victor
+ * @version Nov 4, 2024
+ */
+public class RunLocalTest {
+    public class TestCase {
+      
+    }
+}
