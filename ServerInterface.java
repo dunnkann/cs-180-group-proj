5cd2653fae6a5b151b8a632b6d558f3d175e158b
Kiefhoy <khoy@purdue.edu>
2024-11-18 23:53:26 -0500

    Create ServerInterface.java

---
+++ ServerInterface.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public interface ServerInterface {
+
+    // Starts the server and listens for incoming client connections
+    void startServer(int port) throws IOException;
+
+    // Accepts a client connection and returns a Socket object representing that connection
+    Socket acceptClientConnection() throws IOException;
+
+    // Stops the server and cleans up resources (e.g., closes the server socket)
+    void stopServer() throws IOException;
+}
